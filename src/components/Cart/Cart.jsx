c5cf4bf50052198ec12360d09dfb4ff436f96df2
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2024-08-06 13:59:33 +1000

    cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,34 @@
 import { CartContext } from "../../context/CartContextProvider";
 import { useContext } from "react";
+import { ApiContext } from "../../context/ApiContextProvider";
 
 const Cart = () => {
   const { cart, editCart } = useContext(CartContext);
-  console.log(cart, "cart");
+  const { data } = useContext(ApiContext);
+
+  const findItemData = (id) => {
+    return data.find((item) => item.id === id);
+  };
 
   return (
     <div>
-      {cart.map((el) => (
-        <div>
-          <div>{el.id}</div>
-          <div>{el.quantity}</div>
-        </div>
-      ))}
+      {cart.length > 0 ? (
+        cart.map((item) => {
+          const itemData = findItemData(item.id); // Fetch the object data based on cart item id
+
+          return (
+            <div key={item.id} className="cart-item">
+              <div>Item ID: {item.id}</div>
+              <div>Quantity: {item.quantity}</div>
+              <div>Name: {itemData.title}</div>
+              <div>Price: ${itemData.price}</div>{" "}
+              <button onClick={() => editCart(item.id)}>Add one more</button>
+            </div>
+          );
+        })
+      ) : (
+        <div>Your cart is empty.</div>
+      )}
     </div>
   );
 };
